a7e2bfe1f64bc7861f323102702d5a88816d4308
muskansangwan-coder <muskansangwan225@gmail.com>
2026-07-20 22:06:23 +0530

    Time: 166 ms (72.89%), Space: 12.7 MB (38.97%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -8,13 +8,18 @@ public:
         vector<bool> prime(n, true);
         prime[0] = prime[1] = false;
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i * i < n; i++) {
             if (prime[i]) {
-                count++;
+               
 
-                for (int j = i * 2; j < n; j = j + i)
+                for (int j = i * i; j < n; j = j + i)
                     prime[j] = false;
             }
+        }
+
+        for(int i =2; i<n;i++){
+            if(prime[i])
+                count++;
         }
         return count;
     }
