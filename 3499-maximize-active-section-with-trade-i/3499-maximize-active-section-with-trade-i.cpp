c5effaa4692926d487fe8c4945562090710c547f
muskansangwan-coder <muskansangwan225@gmail.com>
2026-07-22 14:42:53 +0530

    Time: 28 ms (97.77%), Space: 54.9 MB (75%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int totalOnes = 0;
-        vector<int> zeros;
-        int countZeros = 0;
+        int totalOnes = 0 , maxZeros = 0;
+        int prevZeros = 0 , currZeros = 0;
+        bool hasPrev = false;
 
         // traverse string s for counting ones and making segments of zeros
         for (char ch : s) {
             if (ch == '1') {
                 totalOnes++;
-                if (countZeros > 0) {
-                    zeros.push_back(countZeros);
-                    countZeros = 0;
+                if (currZeros > 0) {
+                   if(hasPrev){
+                    maxZeros = max(maxZeros , prevZeros + currZeros);
+                   }
+                   prevZeros = currZeros;
+                   currZeros = 0;
+                   hasPrev = true;
                 }
             } else {
-                countZeros++;
+                currZeros++;
             }
         }
 
         // when last element is zero
-        if (countZeros > 0) 
-            zeros.push_back(countZeros);
+        if (currZeros > 0 && hasPrev) 
+           maxZeros = max(maxZeros , prevZeros + currZeros);
         
 
-        int maxZeros = 0;
-        // checking max value of two continuous segments of zeros
-        if (zeros.size() >= 2){
-            for(int i = 1 ; i<zeros.size(); i++){
-                maxZeros = max(maxZeros , zeros[i-1] + zeros[i]);
-            }
-        }
+        
         return totalOnes + maxZeros;
     }
 };
