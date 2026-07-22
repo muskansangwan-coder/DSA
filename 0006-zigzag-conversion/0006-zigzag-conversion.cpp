1a4d2a114309d52d54a5c38eda9a5f60169dd4dd
muskansangwan-coder <muskansangwan225@gmail.com>
2026-07-22 16:50:55 +0530

    Time: 65 ms (5.02%), Space: 43 MB (5.09%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows<= 1)
+            return s;
+
+        if(numRows > s.size())
+            return s;
+
+        vector<vector<char>> vec(numRows , vector<char>(s.size() , ' '));
+        int index = 0 , col =0, i = 0;
+
+        while (index < s.size()) {
+            for (i = 0; i < numRows && index < s.size(); i++) {
+                vec[i][col] = s[index++];
+            }
+            col++;
+            i -= 2;
+            while(i>0 && index < s.size()){
+                vec[i--][col++] = s[index++];
+            }
+        }
+
+        int cols = s.size();
+        string str = "";
+
+        for(i = 0; i<numRows ; i++){
+            for(int j =0; j< cols; j++){
+                char ch = vec[i][j];
+                if(ch != ' '){
+                    str.push_back(ch);
+                }
+            }
+        }
+        return str;
+    }
+};
