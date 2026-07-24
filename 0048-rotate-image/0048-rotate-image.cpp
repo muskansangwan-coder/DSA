a13720af35eb0bb00a089b2796083fe97a5e9e91
Muskan <muskansangwan225@gmail.com>
2026-07-24 15:16:45 +0530

    Time: 0 ms (100%), Space: 10.3 MB (37.78%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,19 +4,23 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
 
-        // transpose of matrix
-        for (int i = 0; i < rows; i++) {
-            for (int j = i + 1; j < cols; j++) {
-                swap(matrix[i][j], matrix[j][i]);
+        // transpose matrix
+        for(int i =0;i<rows;i++){
+            for(int j = i+1; j<cols;j++){
+                swap(matrix[i][j] , matrix[j][i]);
             }
         }
 
-        // reverse rows of matrix
-        for (int i = 0; i < rows; i++) {
-            int st = 0, end = cols - 1;
-            while (st < end) {
-                swap(matrix[i][st++], matrix[i][end--]);
+        int left = 0 , right = cols-1;
+
+        // reverse rows
+        for(int i = 0;i<rows;i++){
+            while(left<right){
+                swap(matrix[i][left] , matrix[i][right]);
+                left++;
+                right--;
             }
+            left = 0 , right = cols-1;
         }
     }
 };
