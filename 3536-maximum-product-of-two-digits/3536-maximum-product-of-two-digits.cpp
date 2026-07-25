8988dbc8e16fbb545ba32bc7e5132e3deca7b8a5
Muskan <muskansangwan225@gmail.com>
2026-07-25 22:19:14 +0530

    Time: 0 ms (100%), Space: 9.4 MB (6.58%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
-    int maxProduct(int n) { 
-        string str = to_string(n) ;
-        sort(str.begin() , str.end());
+    int maxProduct(int n) {
+        // convert to string
+        string str = to_string(n);
+
+        // sort the string
+        sort(str.begin(), str.end());
+
         int s = str.size();
-        int firstMax = str[s-1] - '0'; 
-        int secondMax = str[s-2] - '0'; 
+        
+        // find last two numbers of the string
+        int firstMax = str[s - 1] - '0';
+        int secondMax = str[s - 2] - '0';
 
         return firstMax * secondMax;
     }
