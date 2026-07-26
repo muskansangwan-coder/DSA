04ea222fd4b5d0fcf94e34271560248caa03dc61
Muskan <muskansangwan225@gmail.com>
2026-07-26 13:41:56 +0530

    Time: 2 ms (10.32%), Space: 18.2 MB (5.01%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,21 +4,29 @@ public:
         string prefix = "", match = "";
         int size = 200;
 
+        // find smallest string and its size
         for (string str : strs) {
             if (str.size() < size) {
                 size = str.size();
                 match = str;
             }
         }
+
+        // use variable to check if they match or not
         bool flag = false;
+
+        // check character of each string at index i
         for (int i = 0; i < size; i++) {
             for (string str : strs) {
+                // not match
                 if (str[i] != match[i]) {
                     flag = true;
                 }
             }
+            // if not match no need to check further
             if (flag)
                 break;
+            // if match add it to prefix string
             else
                 prefix += match[i];
         }
