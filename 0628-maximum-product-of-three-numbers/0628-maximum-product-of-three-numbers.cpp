b9036d532a54b8ded58a25e61b8c165c6d16f037
Muskan <muskansangwan225@gmail.com>
2026-07-26 12:32:27 +0530

    Time: 5 ms (55.82%), Space: 31.5 MB (76.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -4,9 +4,12 @@ public:
         sort(nums.begin(), nums.end());
         int n = nums.size();
 
+        // find three largest numbers
         int max1 = nums[n - 1];
         int max2 = nums[n - 2];
         int max3 = nums[n - 3];
+
+        // find two smallest numbers
         int min1 = nums[0];
         int min2 = nums[1];
 
