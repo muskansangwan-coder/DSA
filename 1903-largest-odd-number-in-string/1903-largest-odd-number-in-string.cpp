7600605ecea78425910fa51f11dca210134a34a5
Muskan <muskansangwan225@gmail.com>
2026-07-26 12:30:12 +0530

    Time: 0 ms (100%), Space: 18 MB (17.68%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -4,9 +4,11 @@ public:
         int n = num.size();
 
         for (int i = n - 1; i >= 0; i--) {
-            if ((num[i] - '0') % 2 == 1) {
+
+            // if current digit id odd return num upto current digit
+
+            if ((num[i] - '0') % 2 == 1)
                 return num.substr(0, i + 1);
-            }
         }
         return "";
     }
