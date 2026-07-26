aa12a85050be247b200a679109905271edfa904a
Muskan <muskansangwan225@gmail.com>
2026-07-26 11:32:35 +0530

    Time: 0 ms (100%), Space: 26.1 MB (94.18%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,32 +2,31 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
+        int left = 0, right = n - 1;
+        if (n == 1)
+            return nums[0];
 
-        if(n == 1) return nums[0];
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int num = nums[mid];
 
-        int st = 0, end = n-1;
-        while(st <= end){
-            int mid = st+ (end-st)/2;
-
-            if(mid == 0 && nums[0] != nums[1]) return nums[mid];
-            if(mid == n-1 && nums[n-1] != nums[n-2]) return nums[mid];
-
-            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]) return nums[mid];
-
-            if(mid%2 == 0){
-                if(nums[mid-1] == nums[mid]){
-                    end = mid-2;
-                }
-                else{
-                    st = mid+1;
-                }
-            }
-            else{
-                if(nums[mid-1] == nums[mid]){
-                    st = mid+1;
-                }
-                else{
-                    end = mid-1;
+            if (mid == 0 && num != nums[1])
+                return num;
+            else if (mid == n - 1 && num != nums[n - 2])
+                return num;
+            else if (num != nums[mid - 1] && num != nums[mid + 1])
+                return num;
+            else {
+                if (mid % 2) {
+                    if (num == nums[mid + 1])
+                        right = mid - 1;
+                    else
+                        left = mid + 1;
+                } else {
+                    if (num == nums[mid - 1])
+                        right = mid - 1;
+                    else
+                        left = mid + 1;
                 }
             }
         }
