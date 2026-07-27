2752b19e263a1fa21d0364a9ecede12fd4037593
Muskan <muskansangwan225@gmail.com>
2026-07-27 14:36:51 +0530

    Time: 0 ms (100%), Space: 9.8 MB (95.26%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        // reverse the string
         reverse(s.begin(), s.end());
 
-        int n = s.size(), st = 0, e = 0, i = 0;
+        int index = 0, start = 0;
 
-        while (i < n) {
-            while (i < n && s[i] == ' ')
+        for (int i = 0; i < s.size(); i++) {
+            // if character is space skip it
+            while (i < s.size() && s[i] == ' ') {
                 i++;
-
-            if (i == n)
+            }
+            // if string ends with space then break
+            if (i == s.size())
                 break;
 
-            while (i < n && s[i] != ' ') {
-                s[e++] = s[i++];
+            // if character is not space add it to the right index
+            while (i < s.size() && s[i] != ' ') {
+                s[index++] = s[i++];
             }
-            reverse(s.begin() + st, s.begin() + e);
-            s[e++] = ' ';
-            st = e;
-            i++;
+            // reverse a word
+            reverse(s.begin() + start, s.begin() + index);
+            // add space after the word
+            s[index] = ' ';
+            index++;
+            // set starting position of next word to index
+            start = index;
         }
-        s.resize(e - 1);
+        // reduce size by 1 to remove the space added after the last word of the
+        // string
+        s.resize(index - 1);
+        // return original string
         return s;
     }
 };
