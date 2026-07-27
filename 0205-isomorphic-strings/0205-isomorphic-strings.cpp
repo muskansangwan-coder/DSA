ec66c8ab079ae3b53fbc91cd23775c3c4483b8ff
Muskan <muskansangwan225@gmail.com>
2026-07-27 15:36:23 +0530

    Time: 3 ms (41.02%), Space: 10.6 MB (7.69%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -5,26 +5,33 @@ public:
         vector<int> vec2;
         int count = 1;
 
+        // array to check ASCII character appeared in s before or not
         int seenS[256] = {0};
+
         for (char ch : s) {
+            // new character so give it a number and increment count
             if (seenS[ch] == 0) {
                 seenS[ch] = count++;
             }
-            count++;
+            // add number given to the character in the vector1
             vec1.push_back(seenS[ch]);
         }
 
         count = 1;
 
+        // array to check ASCII character appeared in t before or not
         int seenT[256] = {0};
+
         for (char ch : t) {
+            // new character so give it a number and increment count
             if (seenT[ch] == 0) {
                 seenT[ch] = count++;
             }
-            count++;
+             // add number given to the character in the vector2
             vec2.push_back(seenT[ch]);
         }
 
+        // if both vectors are equal , strings are isomorphic
         return vec1 == vec2;
     }
 };
