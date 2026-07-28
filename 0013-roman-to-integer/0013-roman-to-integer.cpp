616348f62f8a93c8fd0a60310b868c38ac713935
Muskan <muskansangwan225@gmail.com>
2026-07-28 14:53:39 +0530

    Time: 14 ms (11.82%), Space: 13.4 MB (18.33%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char , int> roman = {
-            {'I' , 1},
-            {'V' , 5},
-            {'X' , 10},
-            {'L' , 50},
-            {'C' , 100},
-            {'D' , 500},
-            {'M' , 1000}
-        };
 
-        int number = 0;
-        for(int i = s.size() - 1; i>=0;i--){
-            if(i == s.size() - 1){
-                number += roman[s[i]];
-            }
-            else if(roman[s[i]] < roman[s[i+1]]){
+        /*    Time Complexity    ---    O(N)
+              Space Complexity   ---    O(1)     */
+
+        // cretae a map to store value of roman alphabets
+        unordered_map<char, int> roman = {
+            {'I', 1},   {'V', 5},   {'X', 10},
+            {'L', 50},  {'C', 100}, {'D', 500},
+            {'M', 1000}};
+
+        int n = s.size();
+        // initialize number with last character of s
+        int number = roman[s[n - 1]];
+
+        for (int i = n - 2; i >= 0; i--) {
+            // if current value is less than the next value
+            if (roman[s[i]] < roman[s[i + 1]]) {
                 number -= roman[s[i]];
-            }
-            else{
+            } else {
                 number += roman[s[i]];
             }
         }
