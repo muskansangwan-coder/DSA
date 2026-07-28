f816a02ee3294f072f99933efd16fdc01e5add05
Muskan <muskansangwan225@gmail.com>
2026-07-28 13:48:29 +0530

    Time: 3 ms (5.81%), Space: 8 MB (69.65%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-
-        /*    Time Complexity    ---    O(N ^ 2)
+        
+        /*    Time Complexity    ---    O(N)
               Space Complexity   ---    O(N)     */
 
-
-        // if both strings are not of equal sizes      
-        if (s.size() != goal.size())
+        
+        // if both strings are not of equal length
+        if(s.size() != goal.size())
             return false;
 
-        // if both strings are same
-        if (s == goal)
-            return true;
+        // double s so that it contains all possible rotations
+        string doubled = s + s;
 
-        // double the string so that it contains all roatations
-        s += s;
-
-        // create window of size of string
-        string str = s.substr(0, goal.size());
-
-        // use sliding window and match every window with goal
-        for (int i = goal.size(); i < s.size(); i++) {
-            // delete previous character
-            str.erase(0, 1);
-            // add new character
-            str += s[i];
-
-            // match strings
-            if (str == goal)
-                return true;
-        }
-        return false;
+        // check if goal is a substring of doubled
+        return doubled.find(goal) != string::npos;
     }
 };
