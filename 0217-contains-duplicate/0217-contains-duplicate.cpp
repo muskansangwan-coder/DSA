333d9781c86ea23f57efe8d2330fcea8cd22a8e2
Muskan <muskansangwan225@gmail.com>
2026-07-29 14:40:13 +0530

    Time: 30 ms (81.48%), Space: 82.9 MB (78.78%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+
+        /*    Time Complexity    ---    O(NlogN)
+              Space Complexity   ---    O(1)     */
+
+
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < nums.size() - 1; i++) {
