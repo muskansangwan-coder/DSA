1c7afc2afa584a9253890bef1339d7bdde88e699
Muskan <muskansangwan225@gmail.com>
2026-08-03 14:02:43 +0530

    Time: 0 ms (100%), Space: 8.3 MB (45.66%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+
+        /*    Time Complexity    ---    O(1)
+              Space Complexity   ---    O(1)     */
+
+
+        int n = word.size();
+
+        if(n <= 8){
+            return n;
+        }
+        else if(n <= 16){
+            return 8 + ( n - 8) * 2;
+        }
+        else if(n <= 24){
+            return 24 + (n - 16) * 3;
+        }
+        else{
+            return 48 + (n - 24) * 4;
+        }
+        return 0;
+    }
+};
