2634cb4f092c35740c0b809af8fb1cb927b6c85b
Muskan <muskansangwan225@gmail.com>
2026-08-03 14:03:51 +0530

    Time: 0 ms (100%), Space: 10.4 MB (87.9%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+
+        /*    Time Complexity    ---    O(1)
+              Space Complexity   ---    O(1)     */
+
+
+        // Alice always wins
+        return true; 
+    }
+};
