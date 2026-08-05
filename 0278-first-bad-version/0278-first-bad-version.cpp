f0b2f2183272b8a0bcbaa2a1c55033e97d39fc63
Muskan <muskansangwan225@gmail.com>
2026-08-05 19:36:53 +0530

    Time: 0 ms (100%), Space: 8 MB (5.34%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -0,0 +1,28 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+
+        /*    Time Complexity    ---    O(logN)
+              Space Complexity   ---    O(1)     */
+
+
+        int s=1 , e=n , ans= 0;
+
+        while(s<=e){
+            int mid = s + (e-s)/2;
+
+            if(isBadVersion(mid)){
+                ans=mid;
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+        }
+        
+        return ans;
+    }
+};
